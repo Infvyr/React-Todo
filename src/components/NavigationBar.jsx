54f740ed387e3a860160b4784ea55c7067443946
Infvyr <allfortrashmail@gmail.com>
2021-09-01 22:19:00 +0300

    refactor: menu links and App and Root components

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,21 +1,27 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const NavigationBar = () => {
-    return (
-        <nav className="navigation">
-            <ul>
-                <li>
-                    <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about" activeClassName="active">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+	return (
+		<nav className="navigation">
+			<ul>
+				<li>
+					<NavLink to="/" activeClassName="active" exact={true}>
+						Home
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/about" activeClassName="active">
+						About
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/contact" activeClassName="active">
+						Contact
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
-export default NavigationBar
+export default NavigationBar;
