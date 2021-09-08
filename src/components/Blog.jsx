ba0bcab51ac5594c7587a3648c98b3fd715c3733
Infvyr <allfortrashmail@gmail.com>
2021-09-08 21:50:22 +0300

    refactor: commented blog filter component

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,133 +1,131 @@
-import { useState } from "react";
-import axios from "axios";
-import { useQuery } from "react-query";
-import { RedditPostsAPIContext } from "../context/RedditPostsAPIContext";
+import { useState } from 'react';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { RedditPostsAPIContext } from '../context/RedditPostsAPIContext';
 
-// import BlogFilter from "./BlogFilter";
-import Search from "./Search";
-import RedditArticle from "./RedditArticle";
-import LoadMore from "./LoadMore";
-import BlogFilter from "./BlogFilter";
+import Search from './Search';
+import RedditArticle from './RedditArticle';
+import LoadMore from './LoadMore';
+// import BlogFilter from './BlogFilter';
 
 const Blog = () => {
-  const [search, setSearch] = useState("");
-  const [visible, setVisible] = useState(9);
-  const [filter, setFilter] = useState("default");
+	const [search, setSearch] = useState('');
+	const [visible, setVisible] = useState(9);
+	// const [filter, setFilter] = useState('default');
 
-  const fetchPosts = async () => {
-    try {
-      const response = await axios.get("https://www.reddit.com/r/aww.json");
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const fetchPosts = async () => {
+		try {
+			const response = await axios.get('https://www.reddit.com/r/aww.json');
+			return response.data;
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const {
-    data: posts,
-    isLoading,
-    isError,
-    error,
-    isSuccess,
-  } = useQuery("posts", fetchPosts, {
-    refetchOnWindowFocus: false,
-  });
+	const {
+		data: posts,
+		isLoading,
+		isError,
+		error,
+		isSuccess,
+	} = useQuery('posts', fetchPosts, {
+		refetchOnWindowFocus: false,
+	});
 
-  const handleSearch = e => {
-    let searchedValue = e.target.value;
-    searchedValue = searchedValue.trimStart();
+	const handleSearch = e => {
+		let searchedValue = e.target.value;
+		searchedValue = searchedValue.trimStart();
 
-    setSearch(searchedValue);
-  };
+		setSearch(searchedValue);
+	};
 
-  const filteredData = posts?.data?.children.filter(
-    ({ data: { title } }) => title.toLowerCase().indexOf(search) > -1
-  );
+	const filteredData = posts?.data?.children.filter(
+		({ data: { title } }) => title.toLowerCase().indexOf(search) > -1
+	);
 
-  const filterPosts = () => {
-    switch (filter) {
-      case "asc":
-        return posts.data.children.filter(
-          ({ data: { author } }) => author === "antonb64"
-        );
-      case "desc":
-        return filteredData.sort().reverse();
-      default:
-        return posts;
-    }
-  };
+	// const postsFiltered = () => {
+	// 	switch (filter) {
+	// 		case 'ASC':
+	// 			return filteredData.filter(({ data: { score } }) => score < 200);
+	// 		case 'DESC':
+	// 			return filteredData.filter(
+	// 				({ data: { num_comments } }) => num_comments < 100
+	// 			);
+	// 		default:
+	// 			return filteredData;
+	// 	}
+	// };
 
-  return (
-    <RedditPostsAPIContext.Provider
-      value={{
-        posts,
-        search,
-        setSearch,
-        handleSearch,
-        visible,
-        setVisible,
-        filteredData,
-        filterPosts,
-        filter,
-        setFilter,
-      }}
-    >
-      <>
-        {isLoading && (
-          <div className="loading">Fetching Reddit API data...</div>
-        )}
-        {isError && (
-          <div className="error">
-            {error.message || "Something went wrong!"}
-          </div>
-        )}
+	return (
+		<RedditPostsAPIContext.Provider
+			value={{
+				// posts,
+				// search,
+				setSearch,
+				handleSearch,
+				visible,
+				setVisible,
+				filteredData,
+				// filter,
+				// setFilter,
+				// postsFiltered,
+			}}>
+			<>
+				{isLoading && (
+					<div className="loading">Fetching Reddit API data...</div>
+				)}
+				{isError && (
+					<div className="error">
+						{error.message || 'Something went wrong!'}
+					</div>
+				)}
 
-        {isSuccess && (
-          <>
-            <div className="container">
-              <div className="reddit-blog-panel">
-                <Search />
-                <BlogFilter />
-              </div>
+				{isSuccess && (
+					<>
+						<div className="container">
+							<div className="reddit-blog-panel">
+								<Search />
+								{/* <BlogFilter /> */}
+							</div>
 
-              <div className="reddit-articles">
-                {filteredData
-                  .slice(0, visible)
-                  .map(
-                    ({
-                      data: {
-                        id,
-                        author,
-                        created,
-                        permalink,
-                        title,
-                        num_comments,
-                        score,
-                        total_awards_received,
-                      },
-                    }) => (
-                      <RedditArticle
-                        key={id}
-                        id={id}
-                        author={author}
-                        created={created}
-                        permalink={permalink}
-                        title={title}
-                        num_comments={num_comments}
-                        score={score}
-                        total_awards_received={total_awards_received}
-                      />
-                    )
-                  )}
-              </div>
+							<div className="reddit-articles">
+								{filteredData
+									.slice(0, visible)
+									.map(
+										({
+											data: {
+												id,
+												author,
+												created,
+												permalink,
+												title,
+												num_comments,
+												score,
+												total_awards_received,
+											},
+										}) => (
+											<RedditArticle
+												key={id}
+												id={id}
+												author={author}
+												created={created}
+												permalink={permalink}
+												title={title}
+												num_comments={num_comments}
+												score={score}
+												total_awards_received={total_awards_received}
+											/>
+										)
+									)}
+							</div>
 
-              <LoadMore />
-            </div>
-          </>
-        )}
-      </>
-    </RedditPostsAPIContext.Provider>
-  );
+							<LoadMore />
+						</div>
+					</>
+				)}
+			</>
+		</RedditPostsAPIContext.Provider>
+	);
 };
 
 export default Blog;
--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -1,59 +1,54 @@
-import { useContext } from "react";
-import { RedditPostsAPIContext } from "../context/RedditPostsAPIContext";
+import { useContext } from 'react';
+import { RedditPostsAPIContext } from '../context/RedditPostsAPIContext';
 
 const BlogFilter = () => {
-  const { filter, setFilter, filterPosts } = useContext(RedditPostsAPIContext);
+	const { filter, setFilter, postsFiltered } = useContext(
+		RedditPostsAPIContext
+	);
 
-  return (
-    <div className="reddit-filter">
-      <div className="reddit-filter__by-title reddit-filter-wrapper">
-        <p>Filter by Title:</p>
-        <button
-          type="button"
-          className={`button reddit-btn--az reddit-btn--filter ${
-            filter === "asc" ? "active" : ""
-          }`}
-          onClick={() => {
-            setFilter("asc");
-            filterPosts();
-            console.log(filter);
-          }}
-        >
-          A-Z
-        </button>
-        <button
-          type="button"
-          className={`button reddit-btn--za reddit-btn--filter ${
-            filter === "desc" ? "active" : ""
-          }`}
-          onClick={() => {
-            setFilter("desc");
+	return (
+		<div className="reddit-filter">
+			<div className="reddit-filter__by-title reddit-filter-wrapper">
+				<p>Filter by Title:</p>
+				<button
+					type="button"
+					className={`button reddit-btn--az reddit-btn--filter ${
+						filter === 'ASC' ? 'active' : ''
+					}`}
+					onClick={() => {
+						setFilter('ASC');
+						postsFiltered();
+					}}>
+					A-Z
+				</button>
+				<button
+					type="button"
+					className={`button reddit-btn--za reddit-btn--filter ${
+						filter === 'DESC' ? 'active' : ''
+					}`}
+					onClick={() => {
+						setFilter('DESC');
+						postsFiltered();
+					}}>
+					Z-A
+				</button>
+			</div>
 
-            filterPosts();
-            console.log(filter);
-          }}
-        >
-          Z-A
-        </button>
-      </div>
-
-      <div className="reddit-filter__by-date reddit-filter-wrapper">
-        <p>Filter by Published Date:</p>
-        <button
-          type="button"
-          className="button reddit-btn--newer reddit-btn--filter"
-        >
-          Newer
-        </button>
-        <button
-          type="button"
-          className="button reddit-btn--older reddit-btn--filter"
-        >
-          Older
-        </button>
-      </div>
-    </div>
-  );
+			<div className="reddit-filter__by-date reddit-filter-wrapper">
+				<p>Filter by Published Date:</p>
+				<button
+					type="button"
+					className="button reddit-btn--newer reddit-btn--filter">
+					Newer
+				</button>
+				<button
+					type="button"
+					className="button reddit-btn--older reddit-btn--filter">
+					Older
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default BlogFilter;
